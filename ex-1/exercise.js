4d1d10bca18291c364a457a68ca2ac67e06f903a
zamanbhutta <marham.ayat@gmail.com>
2021-03-02 11:59:26 +0100

    done 1

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,5 +1,12 @@
 // Use this function that is linked to the button to change the background
 
-function set_background() {
 
-}
+    function set_background() {
+        document.getElementsByTagName("p")[0]
+        .style.backgroundColor = "skyblue";
+        document.getElementsByTagName("p")[1]
+        .style.backgroundColor = "orange";
+        document.getElementsByTagName("p")[2]
+        .style.backgroundColor = "brown"; 
+    }
+
